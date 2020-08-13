cda6223ffc6c40425773823ff0096001eadfe92b
Brian Kan <brian@endevstudios.com>
2020-08-13 16:52:03 -0400

    finished pageTemplate and updated main.dart using pageTemplate.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 
+import './pageTemplate.dart';
 import './noButton.dart';
 import './yesButton.dart';
 
@@ -47,102 +48,107 @@ class _HomePageState extends State<HomePage> {
           },
           children: <Widget>[
             // PAGE 1 STARTS HERE ============================
-            Container(
-              child: Stack(
-                alignment: Alignment.bottomCenter,
-                children: <Widget>[
-                  SizedBox(height: 50),
-                  Container(
-                    width: double.infinity,
-                    height: 480,
-                    // color: Colors.teal[200],
-                    decoration: BoxDecoration(
-                      color: Colors.teal[200],
-                      borderRadius: BorderRadius.only(
-                        topLeft: const Radius.circular(20.0),
-                        topRight: const Radius.circular(20.0),
-                      ),
-                    ),
+            Pagetemplate(pageController, pageChanged),
+            // Container(
+            //   child: Stack(
+            //     alignment: Alignment.bottomCenter,
+            //     children: <Widget>[
+            //       SizedBox(height: 50),
+            //       Container(
+            //         width: double.infinity,
+            //         height: 480,
+            //         // color: Colors.teal[200],
+            //         decoration: BoxDecoration(
+            //           color: Colors.teal[200],
+            //           borderRadius: BorderRadius.only(
+            //             topLeft: const Radius.circular(20.0),
+            //             topRight: const Radius.circular(20.0),
+            //           ),
+            //         ),
 
-                    child: Container(
-                      padding: EdgeInsets.all(16.0),
-                      child: Text(
-                        "Please answer the following question 1.",
-                        textAlign: TextAlign.center,
-                        style: TextStyle(
-                          fontSize: 18,
-                          color: Colors.white,
-                        ),
-                      ),
-                    ),
-                  ),
-                  Container(
-                    padding: EdgeInsets.all(16.0),
-                    height: 400,
-                    decoration: BoxDecoration(
-                      color: Colors.teal[50],
-                      borderRadius: BorderRadius.only(
-                        topLeft: const Radius.circular(20.0),
-                        topRight: const Radius.circular(20.0),
-                      ),
-                    ),
-                    child: Column(
-                      children: <Widget>[
-                        // SizedBox(height: 50),
-                        Text(
-                          "Have you ever had a biopsy regarding this area before?",
-                          style: TextStyle(
-                            fontSize: 17,
-                          ),
-                        ),
-                        // SizedBox for space
-                        SizedBox(height: 50),
-                        Yesbutton(pageController, pageChanged),
-                        Nobutton(pageController, pageChanged),
-                      ],
-                    ),
-                  ),
-                ],
-              ),
-            ),
+            //         child: Container(
+            //           padding: EdgeInsets.all(16.0),
+            //           child: Text(
+            //             "Please answer the following question 1.",
+            //             textAlign: TextAlign.center,
+            //             style: TextStyle(
+            //               fontSize: 18,
+            //               color: Colors.white,
+            //             ),
+            //           ),
+            //         ),
+            //       ),
+            //       Container(
+            //         padding: EdgeInsets.all(16.0),
+            //         height: 400,
+            //         decoration: BoxDecoration(
+            //           color: Colors.teal[50],
+            //           borderRadius: BorderRadius.only(
+            //             topLeft: const Radius.circular(20.0),
+            //             topRight: const Radius.circular(20.0),
+            //           ),
+            //         ),
+            //         child: Column(
+            //           children: <Widget>[
+            //             // SizedBox(height: 50),
+            //             Text(
+            //               "Have you ever had a biopsy regarding this area before?",
+            //               style: TextStyle(
+            //                 fontSize: 17,
+            //               ),
+            //             ),
+            //             // SizedBox for space
+            //             SizedBox(height: 50),
+            //             Yesbutton(pageController, pageChanged),
+            //             Nobutton(pageController, pageChanged),
+            //           ],
+            //         ),
+            //       ),
+            //     ],
+            //   ),
+            // ),
             // PAGE 2 STARTS HERE ============================
-            Container(
-              child: Column(
-                children: <Widget>[
-                  Text(
-                    "Please answer the following question 2.",
-                  ),
-                  Text(
-                    "Have you ever had a biopsy regarding this area before?",
-                    style: TextStyle(
-                      fontSize: 20,
-                    ),
-                  ),
-                  SizedBox(height: 50),
-                  Yesbutton(pageController, pageChanged),
-                  Nobutton(pageController, pageChanged),
-                ],
-              ),
-            ),
+            Pagetemplate(pageController, pageChanged),
+            // Container(
+            //   child: Column(
+            //     children: <Widget>[
+            //       Text(
+            //         "Please answer the following question 2.",
+            //       ),
+            //       Text(
+            //         "Have you ever had a biopsy regarding this area before?",
+            //         style: TextStyle(
+            //           fontSize: 20,
+            //         ),
+            //       ),
+            //       SizedBox(height: 50),
+            //       Yesbutton(pageController, pageChanged),
+            //       Nobutton(pageController, pageChanged),
+            //       Pagetemplate(pageController, pageChanged),
+            //     ],
+            //   ),
+            // ),
+
             // PAGE 3 STARTS HERE ============================
-            Container(
-              child: Column(
-                children: <Widget>[
-                  Text(
-                    "Please answer the following question 3.",
-                  ),
-                  Text(
-                    "Have you ever had a biopsy regarding this area before?",
-                    style: TextStyle(
-                      fontSize: 20,
-                    ),
-                  ),
-                  SizedBox(height: 50),
-                  Yesbutton(pageController, pageChanged),
-                  Nobutton(pageController, pageChanged),
-                ],
-              ),
-            )
+            Pagetemplate(pageController, pageChanged),
+            // Container(
+            //   child: Column(
+            //     children: <Widget>[
+            //       Text(
+            //         "Please answer the following question 3.",
+            //       ),
+            //       Text(
+            //         "Have you ever had a biopsy regarding this area before?",
+            //         style: TextStyle(
+            //           fontSize: 20,
+            //         ),
+            //       ),
+            //       SizedBox(height: 50),
+            //       Yesbutton(pageController, pageChanged),
+            //       Nobutton(pageController, pageChanged),
+            //     ],
+            //   ),
+            // )
           ],
         ));
   }
